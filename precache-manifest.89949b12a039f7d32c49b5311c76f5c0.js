0c4571dbe8dcb281ac6e9bf30370269714a8a279
chchen <chchen@twilio.com>
2020-05-20 17:35:43 -0700

    Updates

---
+++ precache-manifest.89949b12a039f7d32c49b5311c76f5c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a81e6e8dc30b01be40bccfeea0c611",
+    "url": "/zhengzhongtu/index.html"
+  },
+  {
+    "revision": "1eaefacc1e2cddefc704",
+    "url": "/zhengzhongtu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a54fba053c7b8a7cfe9f",
+    "url": "/zhengzhongtu/static/js/2.ad452355.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zhengzhongtu/static/js/2.ad452355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eaefacc1e2cddefc704",
+    "url": "/zhengzhongtu/static/js/main.921bbb1e.chunk.js"
+  },
+  {
+    "revision": "3f5f68142268c59efbe0",
+    "url": "/zhengzhongtu/static/js/runtime-main.34a6e6b1.js"
+  }
+]);
